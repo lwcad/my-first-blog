35d09e8b6b3d8590acf3b85ac5e4e4d5b3f72d2e
lwcad <wiesiekl@gmail.com>
2022-03-21 08:15:14 +0100

    lw proby zmian 1

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,9 @@ from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#lw    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('title')
+#lw    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+    
     return render(request, 'blog/post_list.html', {'posts': posts})
     
 def post_detail(request, pk):
