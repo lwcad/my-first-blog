a2cb4ec0da514bb0ee7f0c0e9dd5ec9606d24868
lwcad <wiesiekl@gmail.com>
2022-03-30 14:41:10 +0200

    dodane CSS , detail drw, itp (6)

--- drw_base/urls.py
+++ drw_base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     #path('drw_base/', views.drw_list, name='drw_list'),
     path('', views.drw_list, name='drw_list'),
+    path('drw/<int:pk>/', views.drw_detail, name='drw_detail'),
 ]
--- drw_base/views.py
+++ drw_base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Drw
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def drw_list(request):
     drws = Drw.objects.all().order_by('nr_rys')
-    return render(request, 'drw_base/drw_list.html',{'drws': drws})
+    return render(request, 'drw_list.html',{'drws': drws})
+    
+def drw_detail(request, pk):
+    drw = get_object_or_404(Drw, pk=pk)
+    return render(request, 'drw_base/drw_detail.html', {'drw': drw})    
