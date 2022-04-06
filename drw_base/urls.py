13a6581de8f45d1ac71ff3e1e2e1566738545022
lwcad <wiesiekl@gmail.com>
2022-04-06 14:23:06 +0200

    + viewer plików PDF (9)

--- drw_base/urls.py
+++ drw_base/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     #path('drw_base/', views.drw_list, name='drw_list'),
-    path('',                   views.drw_list,      name='drw_list'      ),
-    path('drw/<int:pk>/',      views.drw_detail,    name='drw_detail'    ),
-    path('drw/new/',           views.drw_new,       name='drw_new'       ),
-    path('drw/<int:pk>/edit/', views.drw_edit,      name='drw_edit'      ),
-    path('drw/search/',        views.drw_search,    name='drw_search'    ),
-    path('drw/szukaj/',        views.get_szuk_lanc, name='get_szuk_lanc' ),
+    path('',                   views.drw_list,          name='drw_list'          ),
+    path('drw/<int:pk>/',      views.drw_detail,        name='drw_detail'        ),
+    path('drw/new/',           views.drw_new,           name='drw_new'           ),
+    path('drw/<int:pk>/edit/', views.drw_edit,          name='drw_edit'          ),
+    path('drw/result/',        views.drw_search_result, name='drw_search_result' ),
+    path('drw/szukaj/',        views.get_szuk_lanc,     name='get_szuk_lanc'     ),
+    path('drw/pdf_view/<int:pk>/',      views.pdf_view,          name='pdf_view'          ),
 ]
--- drw_base/views.py
+++ drw_base/views.py
@@ -5,17 +5,22 @@ from .models import Drw
 from django.shortcuts import render, get_object_or_404
 from .forms import DrwForm
 from .forms import NameForm
-from django.http import HttpResponse, HttpResponseRedirect
+#from django.http import HttpResponse, HttpResponseRedirect
+from django.http import FileResponse, Http404
 
 # Create your views here.
+
+#----------------------------------------------------------------------------
 def drw_list(request):
     drws = Drw.objects.all().order_by('nr_rys')
     return render(request, 'drw_base/drw_list.html',{'drws': drws})
-    
+
+#----------------------------------------------------------------------------    
 def drw_detail(request, pk):
     drw = get_object_or_404(Drw, pk=pk)
     return render(request, 'drw_base/drw_detail.html', {'drw': drw})
     
+#----------------------------------------------------------------------------
 def drw_new(request):
     if request.method == "POST":
         form = DrwForm(request.POST)
@@ -29,6 +34,7 @@ def drw_new(request):
         form = DrwForm()
     return render(request, 'drw_base/drw_edit.html', {'form': form})
 
+#----------------------------------------------------------------------------
 def drw_edit(request, pk):
     drw = get_object_or_404(Drw, pk=pk)
     if request.method == "POST":
@@ -43,34 +49,46 @@ def drw_edit(request, pk):
         form = DrwForm(instance=drw)
     return render(request, 'drw_base/drw_edit.html', {'form': form})
 
-def drw_search(request):
+#----------------------------------------------------------------------------
+def drw_search_result(request):
     global SzukajLancucha
-    #SzukanyLancuch = query
-    #drws = Drw.objects.filter(nr_rys='1-4-3568').order_by('nr_rys')
-    drws = Drw.objects.filter(nr_rys=SzukajLancucha).order_by('nr_rys')
-    
-    return render(request, 'drw_base/drw_search.html',{'drws': drws})
+    #drws = Drw.objects.filter(nr_rys=SzukajLancucha).order_by('nr_rys')
+    drws = Drw.objects.filter(nr_rys__contains=SzukajLancucha).order_by('nr_rys')
+    iloscRysZnalez = len(drws)
+    #return render(request, 'drw_base/drw_search_result.html',{'drws': drws})
+    return render(request, 'drw_base/drw_search_result.html',{'drws': drws,'iloscRysZnalez':iloscRysZnalez} )    
 
-
+#----------------------------------------------------------------------------
 def get_szuk_lanc(request):
     global SzukajLancucha
+    SzukajLancucha = ''
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            #return HttpResponseRedirect('/thanks/')
-            #return HttpResponseRedirect('x')
             SzukajLancucha = form.cleaned_data['LancSzukany']
-            #print(query)            
-            return redirect( 'drw_search' )
+            return redirect( 'drw_search_result' )
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
 
-    return render(request, 'drw_base/drw_search_result.html', {'form': form})
+    return render(request, 'drw_base/drw_search.html', {'form': form})
+
+#----------------------------------------------------------------------------
+def pdf_view(request, pk):
+   
+    drw = get_object_or_404(Drw, pk=pk)
+
+    podstawaSciezki      = 'Y:/rys_arch_nt/'
+    nrArchiwalny         = drw.nr_arch.replace(".", "_")
+    katalog              = nrArchiwalny[0:9] + "_/"
+    pelnaSciezkaPlikuPdf = podstawaSciezki + katalog + nrArchiwalny + ".pdf"
+    
+    try:
+        return FileResponse(open(pelnaSciezkaPlikuPdf, 'rb'), content_type='application/pdf')        
+
+    except FileNotFoundError:
+        raise Http404()
